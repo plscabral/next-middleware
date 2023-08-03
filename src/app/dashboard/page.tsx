b3f836a0fb9cbee7d61f7fd06d13720e5898f325
Paulo Cabral <paulo.luiz127@gmail.com>
2023-08-03 12:15:43 -0300

    .

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
   const router = useRouter();
 
   function handleLogin() {
-    Cookie.set("auth_token", "be5400c3-5ecb-4ac1-b7f6-3bb431096c33")
-    router.push('/dashboard')
+    Cookie.remove("auth_token")
+    router.push('/')
   }
 
   return (
     <div className="flex min-h-screen bg-slate-900 items-center justify-center">
-      <Button onClick={handleLogin} className='bg-green-400'>Login</Button>
+      <Button onClick={handleLogin} className='bg-red-400'>Logout</Button>
     </div>
   )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export default function middleware(request: NextRequest) {
+  const token = request.cookies.get('auth_token')?.value
+
+  const signInURL = new URL('/', request.url)
+  const dashboardURL = new URL('/dashboard', request.url)
+
+  if(!token) {
+    if(request.nextUrl.pathname === "/") {
+      return NextResponse.next()
+    }
+
+    return NextResponse.redirect(signInURL)
+  }
+
+  if(request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(dashboardURL)
+  }
+}
+
+
+export const config = {
+  matcher: ['/', '/dashboard:path']
+}
